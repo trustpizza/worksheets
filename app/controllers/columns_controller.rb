2d41061573a48ff4ab7dc2c0d452d86671f82edc
Axel <maxelolsson@gmail.com>
2023-11-14 11:30:31 -0500

    revert

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,12 +1,5 @@
 class ColumnsController < ApplicationController
 
-    def new
-        respond_to do |format|
-            format.html
-            format.turbo_stream
-        end
-    end
-
     def create
         @sheet = Sheet.find(params[:sheet_id])
         @column = @sheet.columns.create(column_params)
